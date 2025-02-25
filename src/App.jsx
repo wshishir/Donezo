6c3d9176d929e8c62f59b68180292f1ffd79ccb1
shishir <shishirtech506@gmail.com>
2025-02-25 22:57:52 +0530

    added bg

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import React, { useState } from "react";
 
 function App() {
   return (
     <>
-      <Navbar/>
+      <div class="dark">
+        <div className="h-screen w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
+          {/* Radial gradient for the container to give a faded look */}
+          <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+        </div>
+      </div>
     </>
   );
 }
