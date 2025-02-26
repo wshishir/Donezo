991ce1a9bf79e4e819509d10336b66cc8605b26c
shishir <shishirtech506@gmail.com>
2025-02-27 03:20:02 +0530

    added foreground

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import React, { useState } from "react";
 import Background from "./components/Background";
+import Foreground from "./components/Foreground";
+
 function App() {
   return (
     <>
       <div className='bg-neutral-900 h-screen w-full relative'>
         <Background/>
+        <Foreground/>
       </div>
     </>
   );
---
+++ src/components/Foreground.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Foreground = () => {
+  return <div className='fixed top-0 left-0 z-[3] w-full h-full'></div>;
+};
+
+export default Foreground;
