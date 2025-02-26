e41b71c04caecd5f6531e50542f20d553552250b
shishir <shishirtech506@gmail.com>
2025-02-27 03:27:31 +0530

    card

--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Foreground = () => {
-  return <div className='fixed top-0 left-0 z-[3] w-full h-full'></div>;
+  return (
+    <div className='fixed top-0 left-0 z-[3] w-full h-full'>
+      <div className='w-60 h-72 bg-neutral-400  rounded-[20px]'></div>
+    </div>
+  );
 };
 
 export default Foreground;
