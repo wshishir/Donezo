03ccada73ad027b7aeb0f32cd85cdc2460fc1d49
shishir <shishirtech506@gmail.com>
2025-02-27 02:56:26 +0530

    added comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import "./App.css";
 import React, { useState } from "react";
+import Background from "./components/Background";
 function App() {
   return (
     <>
       <div className='bg-neutral-900 h-screen w-full relative'>
-        <div className=' absolute top-[5%] w-full py-10  flex justify-center text-zinc-600'>
-          Documents
-        </div>
-        <h1 className=' absolute font-title top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-7xl text-neutral-600 hover:text-neutral-500'>
-          Donezo
-        </h1>
+        <Background/>
       </div>
     </>
   );
